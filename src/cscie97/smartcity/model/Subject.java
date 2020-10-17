227260e128f490374e5e7aa074be3755f8473447
Stephen <20730278+ssheld@users.noreply.github.com>
2020-10-17 18:25:33 -0400

    Changed to interface

--- src/cscie97/smartcity/model/Subject.java
+++ src/cscie97/smartcity/model/Subject.java
@@ -1,39 +1,26 @@
 package cscie97.smartcity.model;
 
-import cscie97.smartcity.controller.Observer;
-
-import java.util.List;
-
 /**
  * Author: Stephen Sheldon
  **/
-public abstract class Subject {
+public interface Subject {
 
-    private List<Observer> observerList;
 
     /**
      * Register an observer with this subject.
      * @param o  The Observer object to register.
      */
-    public void registerObserver(Observer o) {
-        observerList.add(o);
-    }
+    void registerObserver(Observer o);
 
     /**
      * Remove an observer from watching this subject.
      * @param o  The observer to remove from watching the subject.
      */
-    public void removeObserver(Observer o) {
-        observerList.remove(o);
-    }
+    void removeObserver(Observer o);
 
     /**
      * Notify all the observers of a new SensorEvent.
      * @param event  The SensorEvent to notify observers of.
      */
-    public void notifyObservers(SensorEvent event) {
-        for (Observer observer : observerList) {
-            observer.update(event);
-        }
-    }
+    void notifyObservers(SensorEvent event);
 }
