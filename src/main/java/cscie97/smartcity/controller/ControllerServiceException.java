cc69dabbe7c6b33adede1522d6b70d59eb565a0d
Stephen <20730278+ssheld@users.noreply.github.com>
2020-10-23 08:08:26 -0400

    Created ControllerServiceException

--- src/main/java/cscie97/smartcity/controller/ControllerServiceException.java
+++ src/main/java/cscie97/smartcity/controller/ControllerServiceException.java
@@ -3,5 +3,32 @@ package cscie97.smartcity.controller;
 /**
  * Author: Stephen Sheldon
  **/
-public class ControllerServiceException {
+public class ControllerServiceException extends Exception {
+    /**
+     * Action that was performed.
+     */
+    private String reason;
+
+    /**
+     * Reason for the exception.
+     */
+    private String action;
+
+    /**
+     * Constructor method.
+     * @param reason Reason for the exception.
+     * @param action Action that was performed.
+     */
+    public ControllerServiceException(String reason, String action) {
+        this.reason = reason;
+        this.action = action;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getAction() {
+        return action;
+    }
 }
